3b64407987a769972e1142b88933faa07cfb21be
Łukasz Jazgar <lukasz.jazgar@miquido.com>
2018-03-26 17:33:55 +0200

    Change user of dockers

--- standardHooks/build-nodejs.sh
+++ standardHooks/build-nodejs.sh
@@ -18,4 +18,4 @@
 # DEPLOYMENT_CONF_DIR
 
 # Prepare dependencies
-docker run -v $PROJECT_DIR:/usr/src/app -w /usr/src/app -t node npm run build:$ENV
+docker run -v $PROJECT_DIR:/usr/src/app -w /usr/src/app -t -u=$UID node npm run build:$ENV
